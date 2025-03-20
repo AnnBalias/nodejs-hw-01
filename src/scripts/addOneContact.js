3bbe83456f5708f4eb8e66865e24cea54c6b7d1a
AnnBalias <annbalias@gmail.com>
2025-03-20 16:38:48 +0200

    homework-1

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,12 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contactsArr = await readContacts();
+  const newContact = createFakeContact();
+  const newContactsArr = [...contactsArr, newContact];
+  writeContacts(newContactsArr);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsArr = await readContacts();
+  const newContacts = Array(number)
+    .fill(0)
+    .map(() => createFakeContact());
+  const newContactsArr = [...contactsArr, ...newContacts];
+  writeContacts(newContactsArr);
+};
 
 generateContacts(5);
